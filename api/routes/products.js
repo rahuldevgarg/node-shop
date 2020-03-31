d15a10d90705131a02ed2e2c32f2a79ba7b3d244
Rahul Dev Garg <rdg042000@gmail.com>
2020-03-31 10:32:44 +0530

    added different routes to server

---
+++ api/routes/products.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6*/ 
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res, next)=>{
+    res.status(200).json({
+        message:'Handling GET requests to /products'
+    });
+});
+
+router.post('/',(req, res, next)=>{
+    res.status(200).json({
+        message:'Handling POST requests to /products'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
-
+const productRoutes = require('./api/routes/products');
 app.use('/demo',(req, res, next) => {
     res.status(200).json(
         {
-            message:"It Works",
-            txt : "Rahul Dev Garg"
+            message:"You are viewing demo.",
+            author : "Rahul Dev Garg"
         }
     );
 });
+app.use('/products',productRoutes);
 
 module.exports = app;

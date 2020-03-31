18abe63650972b7ba77907f03a6d5a69311d2177
Rahul Dev Garg <rdg042000@gmail.com>
2020-03-31 16:57:09 +0530

    added body parser

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/demo',(req, res, next) => {
     res.status(200).json(
@@ -16,5 +20,20 @@ app.use('/demo',(req, res, next) => {
 //Routes which should handle requests
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+
+app.use( (req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message,
+        }
+    });
+});
 
 module.exports = app;
